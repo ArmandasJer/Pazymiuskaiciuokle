cc96a9061d7b332c8966433e970ec02cd7c1a694
ArmandasJer <85392121+ArmandasJer@users.noreply.github.com>
2021-06-18 06:35:56 +0300

    Create add.h

---
+++ add.h
@@ -0,0 +1,29 @@
+#ifndef ADD_H_INCLUDED
+#define ADD_H_INCLUDED
+#include <bits/stdc++.h>
+#include<limits>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::swap;
+using std::numeric_limits;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::ifstream;
+using std::stringstream;
+using std::ofstream;
+struct pazymiai
+{
+    string vardas, pavarde;
+    vector <int> pazymys = {};
+    double galutinis;
+    pazymiai()
+    {
+        galutinis = 0;
+    }
+};
+#endif // ADD_H_UNCLUDED
